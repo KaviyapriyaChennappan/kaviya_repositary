793b86566b81308f2dc9eaa7bdaddb4f470285d5
KaviyapriyaChennappan <kaviyapriyac@gmail.com>
2025-01-26 14:04:25 -0500

    commit

--- src/ArrayMaximumOfGivenNumbers.java
+++ src/ArrayMaximumOfGivenNumbers.java
@@ -2,12 +2,7 @@ import java.util.Scanner;
 
 public class ArrayMaximumOfGivenNumbers
 {
-    public void compare() {
-        int arr[] = new int[10];
-        Scanner s = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = s.nextInt();
-        }
+    public void compare(int[] arr) {
         int big = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (big < arr[i]) {
--- src/ArrayMaximumOfGivenNumbersMain.java
+++ src/ArrayMaximumOfGivenNumbersMain.java
@@ -1,6 +1,16 @@
-public class ArrayMaximumOfGivenNumbersMain {
-    public static void main(String[] args) {
-ArrayMaximumOfGivenNumbers a = new ArrayMaximumOfGivenNumbers();
-a.compare();
+import java.util.Scanner;
+
+public class ArrayMaximumOfGivenNumbersMain
+{
+    public static void main(String[] args)
+    {
+        int arr[] = new int[10];
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = s.nextInt();
+        }
+        ArrayMaximumOfGivenNumbers a = new ArrayMaximumOfGivenNumbers();
+        a.compare(arr);
     }
 }
