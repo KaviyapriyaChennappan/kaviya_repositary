7fe41ff0bf7ebab08111408e63ead7dd987c2c0e
KaviyapriyaChennappan <kaviyapriyac@gmail.com>
2025-01-31 17:41:20 -0500

    commit

---
+++ src/StringDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String s;
+        System.out.println("Enter a String");
+        Scanner sc = new Scanner(System.in);
+        s= sc.nextLine();
+        System.out.println("LOWER_CAEE" + s.toLowerCase());
+        System.out.println("LENGTH"+s.length());
+        System.out.println("UPPERCASE"+s.toUpperCase());
+        System.out.println("CHAR AT"+ s.charAt(0));
+        System.out.println("SUBSTRING"+ s.substring(0,2));
+        char []  chars = s.toCharArray();
+        String concat  = s.concat(" Welcome!");
+        System.out.println(concat);
+    }
+}
