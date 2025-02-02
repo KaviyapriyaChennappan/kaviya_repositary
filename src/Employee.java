336a478deabe92637822efcc0498f42c368e7432
KaviyapriyaChennappan <kaviyapriyac@gmail.com>
2025-02-02 16:16:39 -0500

    commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringDemo {
+public class Employee {
     public static void main(String[] args) {
         String s;
         System.out.println("Enter a String");
---
+++ src/StaticDemo.java
@@ -0,0 +1,4 @@
+public class StaticDemo {
+
+    int x = 10;
+}
